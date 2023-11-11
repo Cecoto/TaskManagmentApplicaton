b08174d1a3f459448094cb9fd4ccc129755e7275
CECO <cvetomir97@abv.bg>
2023-11-11 21:56:35 +0200

    Small changes made.

--- TaskManagement.DTOs/TaskTypeDto.cs
+++ TaskManagement.DTOs/TaskTypeDto.cs
@@ -2,11 +2,16 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class TaskTypeDto
+    public class TaskTypeDto
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
     }
 }
